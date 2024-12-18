3d248d32b7f7a15123944a13416afe8efd33b36e
kimsayhi <kimse418@gmail.com>
2024-12-18 09:59:40 +0900

    style: css 수정

--- src/app/scroll-and-show/page.tsx
+++ src/app/scroll-and-show/page.tsx
@@ -10,33 +10,34 @@ export default function ScrollAndShowPage() {
     setBallSize(latestValue * 100);
   });
   return (
-    <div className="h-[3000px]">
+    <div className="h-[300vh]">
       <motion.span
         style={{ clipPath: `circle(${ballSize}%)` }}
         className="fixed w-screen h-screen top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-400 "
-      ></motion.span>
-      <div className="fixed flex flex-col top-[10%] left-[10%] text-4xl font-bold text-blue-800">
-        <div className="overflow-hidden">
-          <motion.p
-            initial={{ bottom: "-150px" }}
-            animate={{ bottom: ballSize > 70 ? "0" : "-150px" }}
-            transition={{ type: "spring" }}
-            className="relative"
-          >
-            Aha!
-          </motion.p>
-        </div>
-        <div className="overflow-hidden">
-          <motion.p
-            initial={{ bottom: "-150px" }}
-            animate={{ bottom: ballSize > 70 ? "0" : "-150px" }}
-            transition={{ type: "spring" }}
-            className="relative overflow-hidden"
-          >
-            You found me!
-          </motion.p>
-        </div>
-      </div>
+      >
+        <h1 className=" flex flex-col text-[8vw] pl-[8vw] font-bold text-blue-800">
+          <span className="overflow-hidden">
+            <motion.span
+              initial={{ bottom: "-150px" }}
+              animate={{ bottom: ballSize > 70 ? "0" : "-150px" }}
+              transition={{ type: "spring" }}
+              className="relative"
+            >
+              Aha!
+            </motion.span>
+          </span>
+          <span className="overflow-hidden">
+            <motion.span
+              initial={{ bottom: "-150px" }}
+              animate={{ bottom: ballSize > 70 ? "0" : "-150px" }}
+              transition={{ type: "spring" }}
+              className="relative overflow-hidden"
+            >
+              You found me!
+            </motion.span>
+          </span>
+        </h1>
+      </motion.span>
     </div>
   );
 }
